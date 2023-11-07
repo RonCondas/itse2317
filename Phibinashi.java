059c3c72f6c71a81ad568afaa942a4b820912ad2
Ronald Noriega <124211322+RonCondas@users.noreply.github.com>
2023-11-07 12:31:06 -0600

    Add files via upload

---
+++ Phibinashi.java
@@ -0,0 +1,17 @@
+
+public class Phibinashi {
+
+	public static void main(String[] args) 
+	{
+		String str = "Hello";
+		phibinachi();
+
+	}
+
+	private static void phibinachi() {
+		
+		
+		phibinachi();
+	}
+
+}
